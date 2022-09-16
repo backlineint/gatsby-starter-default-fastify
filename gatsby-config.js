1237249caa2639d1de59ca81a8e6240893bbf809
Brian Perry <brian.perry@pantheon.io>
2022-09-16 15:23:41 -0500

    Default fastify plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-fastify`,
+      options: {}, // All options are optional
+    },
   ],
 }
